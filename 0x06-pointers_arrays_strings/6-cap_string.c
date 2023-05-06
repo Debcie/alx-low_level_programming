9d3d69d853ff863fe8742381cd22b1913d83e752
debcie <deborahthedoc@gmail.com>
2023-05-05 21:32:35 -0700

    7-leet.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,35 +2,30 @@
 
 /**
   * cap_string - function that capitalizes all words of a string
-  * @str: string
-  * Return: pointer to string
+  * @str: input string
+  * Return: pointer to destination of string
   */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[i])
-	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+	if (*(str + count) >= 97 && *(str + count) <= 122)
+		*(str + count = *(str + count) - 32;
+				count++;
+				while (*(str + count) != '\0')
+				{
+					for (i = 0; i < 13; i++)
+					{
+					if (*(str + count) == sep_words[i])
+						{
 
-			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] ==  '\t' ||
-				str[i - 1] ==  '\n' ||
-				str[i - 1] ==  ',' ||
-				str[i - 1] ==  ';' ||
-				str[i - 1] ==  '.' ||
-				str[i - 1] ==  '!' ||
-				str[i - 1] ==  '?' ||
-				str[i - 1] ==  '"' ||
-				str[i - 1] ==  '(' ||
-				str[i - 1] ==  ')' ||
-				str[i - 1] ==  '{' ||
-				str[i - 1] ==  '}' ||
-				i == 0)
-			str[i] -= 42;
-
-		i++;
-	}
-		return (str);
+					if ((*(str + (count + 1)) >= 97) && (*(str + count + 1)) <= 122))
+						*(s count + 1)) = *(s + (count + 1)) - 32;
+					break;
+						}
+					}
+					count++;
+				}
+				return (str);
 }
